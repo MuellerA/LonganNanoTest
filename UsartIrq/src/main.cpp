b716462a3f16ea3b27ecb53aae66c260a90875cf
MuellerA <MuellerA@users.noreply.github.com>
2020-02-29 17:45:50 +0100

    use lib

--- UsartIrq/src/main.cpp
+++ UsartIrq/src/main.cpp
@@ -7,31 +7,36 @@ extern "C"
 #include "gd32vf103.h"
 }
 
-#include "delay.h"
-#include "spi.h"
-#include "lcd.h"
+#include "GD32VF103/time.h"
+#include "Longan/lcd.h"
+#include "Longan/fonts.h"
+
+using ::RV::GD32VF103::TickTimer ;
+using ::RV::Longan::Lcd ;
+using ::RV::Longan::LcdArea ;
+
 #include "usart.h"
 
 extern "C" const uint8_t font[1520] ;
 
-Spi0 spi ;
-Lcd lcd{spi, font, 16, 8} ;
+Lcd& lcd{Lcd::lcd()} ;
 Usart0 usart ;
 
-void putc(char ch)
+void putc(LcdArea &la, char ch)
 {
   usart.put((uint8_t)ch) ;
-  lcd.putChar(ch) ;
+  la.put(ch) ;
 }
 
 int main()
 {
-  spi.setup() ;
   lcd.setup() ;
   usart.setup(115200UL) ;
 
+  LcdArea la{lcd, 0, 160, 16, 64} ;
+  
   for (char i : "Hallo Internet!\n")
-    putc(i) ;
+    putc(lcd, i) ;
   
   while (true)
   {
@@ -39,8 +44,8 @@ int main()
     
     if (usart.get(ch))
     {
-      putc(ch) ;
-      delayMs(50) ;
+      putc(la, ch) ;
+      TickTimer::delayMs(50) ;
     }
   }
 }
